37a8b3b6641fb9335b49f538a042367e80dabe83
ShinProg (Logan Tann) <logon313@hotmail.fr>
2023-04-11 18:46:07 +0200

    feat: implement Grid component and add playground route

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,5 +6,13 @@ export default <Partial<Config>>{
         fontFamily: {
             sans: 'Raleway',
         },
-    }
+    },
+    safelist: [
+        "grid",
+        "grid-cols-12",
+        {
+            pattern: /col-span-(1|2|3|4|5|6|7|8|9|10|11|12)/,
+            variants: ['md', 'lg', 'xl', '2xl']
+        }
+    ]
 }

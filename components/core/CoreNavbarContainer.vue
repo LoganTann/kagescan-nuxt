2d71a880c0dadbc28f61a3185b98d217370c4dba
ShinProg (Logan Tann) <logon313@hotmail.fr>
2023-04-24 13:26:42 +0200

    feat(core): create container component

--- components/core/CoreNavbarContainer.vue
+++ components/core/CoreNavbarContainer.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="bg-white/75" :class="navClasses">
-        <div class="flex items-center h-20 mx-auto md:w-11/12 lg:w-9/12 max-w-7xl px-1 sm:px-3 md:px-10">
+        <CoreContainer class="flex items-center h-20">
             <div class="flex items-center flex-1 justify-center md:justify-start">
                 <NuxtLink to="/">
                     <img
@@ -11,10 +11,7 @@
                 </NuxtLink>
             </div>
             <CoreNavbarFloatingLinks class="h-full hidden md:flex items-center justify-center"></CoreNavbarFloatingLinks>
-            <!--<div class="md:flex-1 flex items-center justify-end"> -->
-            <!-- Est-ce qu'il y aura un truc ici un jour ? 
-            </div>-->
-        </div>
+        </CoreContainer>
     </nav>
 </template>
 <script setup lang="ts">
--- pages/playground.vue
+++ pages/playground.vue
@@ -1,6 +1,9 @@
 <template>
     <main class="">
-        <DevLorem count="30"></DevLorem>
+        <CoreContainer>
+            <DevLorem count="30"></DevLorem>
+        </CoreContainer>
+
         <Grid row>
             <Grid col="s12 m8 l3 push-l3" class="border-4 border-blue-800"> toto </Grid>
             <Grid col="s12 m4 l3 push-l3" class="border-4 border-red-500"> toto2 </Grid>

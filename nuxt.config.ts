700960c18d663e17494ce67a3b22c88eb3c0b3ed
ShinProg (Logan Tann) <logon313@hotmail.fr>
2023-05-16 16:22:46 +0200

    feat: add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,11 @@ export default defineNuxtConfig({
                     //href: 'https://fonts.googleapis.com/css2?family=Raleway&display=swap'
                     href: 'https://fonts.googleapis.com/css2?family=Open+Sans'
                 },
-                { rel: 'preconnect', href: 'https://fonts.gstatic.com' }
+                { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+                { rel: 'manifest', href: '/site.webmanifest' },     
             ],
         }
     },

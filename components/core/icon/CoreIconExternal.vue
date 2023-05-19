83a2ae8c33beaf21177789cd697e517f543c365c
ShinProg (Logan Tann) <logon313@hotmail.fr>
2023-05-19 02:07:25 +0200

    feat: ajout icones externe

---
+++ components/core/icon/CoreIconExternal.vue
@@ -0,0 +1,14 @@
+<template>
+    <font-awesome-icon
+        v-if="!autodetect || autodetect.startsWith('http')"
+        class="ml-1 text-gray-400"
+        icon="fa-solid fa-arrow-up-right-from-square"
+        size="2xs"
+        style="vertical-align: 0"
+    />
+</template>
+<script setup lang="ts">
+    const props = defineProps<{
+        autodetect?: string;
+    }>();
+</script>
